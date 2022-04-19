a41febe0d269ef3c591efe34a91950d9008f57e6
snehalkorade9 <snehal.korade99@gmail.com>
2022-04-20 02:00:49 +0530

    all tests updated

--- tests/click_test.py
+++ tests/click_test.py
@@ -1,5 +1,6 @@
 import os
 
+import sys
 from click.testing import CliRunner
 
 from app import create_log_folder, create_database
@@ -8,11 +9,12 @@ runner = CliRunner()
 
 
 def test_create_log_folder():
-    response = runner.invoke(create_log_folder)
-    assert response.exit_code == 0
-    root = os.path.dirname(os.path.abspath(__file__))
+    response = runner.invoke(create_log_folder, [])
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print(root)
     # set the name of the apps log folder to logs
-    logdir = os.path.join(root, '../logs')
+    logdir = os.path.join(root, './/app/logs')
+    print(logdir)
     # make a directory if it doesn't exist
     assert os.path.exists(logdir) == True
 
@@ -24,3 +26,46 @@ def test_create_database():
     dbdir = os.path.join(root, '../database')
     # make a directory if it doesn't exist
     assert os.path.exists(dbdir) == True
+
+def test_create_log_flask():
+    # set the name of the apps log folder to logs
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, './/app/logs', 'flask.log')
+    # make a directory if it doesn't exist
+    assert os.path.exists(logdir) == True
+
+def test_create_log_errors():
+    # set the name of the apps log folder to logs
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, './/app/logs', 'errors.log')
+    # make a directory if it doesn't exist
+    assert os.path.exists(logdir) == True
+
+def test_create_log_myapp():
+    # set the name of the apps log folder to logs
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, './/app/logs', 'myapp.log')
+    print(logdir)
+    # make a directory if it doesn't exist
+    assert os.path.exists(logdir) == True
+
+def test_create_log_sqlalchemy():
+    # set the name of the apps log folder to logs
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, './/app/logs', 'sqlalchemy.log')
+    print(logdir)
+    # make a directory if it doesn't exist
+    assert os.path.exists(logdir) == True
+
+def test_create_log_werkzeug():
+    # set the name of the apps log folder to logs
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, './/app/logs', 'sqlalchemy.log')
+    print(logdir)
+    # make a directory if it doesn't exist
+    assert os.path.exists(logdir) == True
